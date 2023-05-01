0a3eb0efd634d3e277e9e6927fc46559b50e5a36
cyace84 <cyace84@gmail.com>
2023-05-01 05:37:10 +0200

    rebuild sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,11 +2,40 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import typescript from '@rollup/plugin-typescript';
 
+import nodeExternals from 'vite-plugin-node-externals';
+import { viteExternalsPlugin as externals } from 'vite-plugin-externals';
+
 module.exports = {
   title: 'Everscale Inpage Provider',
   base: '/everscale-inpage-provider/docs/',
   description: 'Web3-like interface to the Everscale blockchain.',
-  plugins: [vue(), typescript()],
+  plugins: [
+    vue(),
+    typescript(),
+    nodeExternals({
+      allowList: ['vue', 'vue-toastification', 'js-base64'],
+    }),
+    // externals({
+    //   externals: ['fs', 'path', 'assert', 'util', 'typedoc'],
+    // }),
+  ],
+  // resolve: {
+  //   alias: {
+  //     alias: {
+  //       http: require.resolve('rollup-plugin-node-builtins'),
+  //       path: require.resolve('rollup-plugin-node-builtins'),
+  //       fs: require.resolve('rollup-plugin-node-builtins'),
+  //       os: require.resolve('rollup-plugin-node-builtins'),
+  //       tslib: require.resolve('rollup-plugin-node-builtins'),
+  //       child_process: require.resolve('rollup-plugin-node-builtins'),
+  //       crypto: require.resolve('rollup-plugin-node-builtins'),
+  //       stream: require.resolve('rollup-plugin-node-builtins'),
+  //       https: require.resolve('rollup-plugin-node-builtins'),
+  //       http2: require.resolve('rollup-plugin-node-builtins'),
+  //       process: require.resolve('rollup-plugin-node-builtins'),
+  //     },
+  //   },
+  // },
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
@@ -21,16 +50,11 @@ module.exports = {
         items: [
           { text: 'Introduction', link: '/pages/guides/1-introduction.md' },
           { text: 'Installation and Configuration', link: '/pages/guides/2-installation.md' },
-          {
-            text: 'Core Components',
-            collapsable: false,
-            items: [
-              { text: 'Working with Cells', link: '/pages/guides/3.1-working-with-cells.md' },
-              { text: 'Interaction with Contracts', link: '/pages/guides/3.2-working-with-contracts.md' },
-              { text: 'Subscriptions', link: '/pages/guides/3.3-subscriptions.md' },
-              { text: 'Cryptography & Security', link: '/pages/guides/3.4-cryptography.md' },
-            ],
-          },
+          { text: 'Working with Cells', link: '/pages/guides/3.1-working-with-cells.md' },
+          { text: 'Interaction with Contracts', link: '/pages/guides/3.2-working-with-contracts.md' },
+          { text: 'Subscriptions', link: '/pages/guides/3.3-subscriptions.md' },
+          { text: 'Cryptography & Security', link: '/pages/guides/3.4-cryptography.md' },
+
           // { text: 'Standalone Section', link: '/pages/standalone-section.md' },
           // {
           //   text: 'Real-life Examples',
@@ -67,15 +91,16 @@ module.exports = {
       },
     ],
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        lightTheme: resolve(__dirname, 'styles/light-theme.css'),
-        darkTheme: resolve(__dirname, 'styles/dark-theme.css'),
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+  //       lightTheme: resolve(__dirname, 'styles/light-theme.css'),
+  //       darkTheme: resolve(__dirname, 'styles/dark-theme.css'),
+  //       external: ['fs', 'path', 'assert', 'util'],
+  //     },
+  //   },
+  // },
 
   esbuild: {
     target: ['chrome89', 'edge89', 'firefox79', 'safari14.1'],
