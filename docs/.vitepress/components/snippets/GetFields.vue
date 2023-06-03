4eb96bb835c73f51ef74dfaaa65711ef4adb4ba6
cyace84 <cyace84@gmail.com>
2023-06-03 17:20:21 +0200

    added getField guide

---
+++ docs/.vitepress/components/snippets/GetFields.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="demo">
+    <button @click="getContractFields">getFields</button>
+    <pre class="contract-fields" v-if="contractFields">Contract Fields: {{ contractFields }}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { Address, ProviderRpcClient } from 'everscale-inpage-provider';
+
+import { testContract } from './../../helpers';
+
+export default defineComponent({
+  name: 'GetContractFields',
+  setup() {
+    const contractFields = ref('');
+
+    return { contractFields };
+  },
+  methods: {
+    async getContractFields() {
+      const provider = new ProviderRpcClient();
+      await provider.ensureInitialized();
+
+      await provider.requestPermissions({
+        permissions: ['basic'],
+      });
+
+      const example = new provider.Contract(testContract.ABI, new Address(testContract.address));
+      const { fields, state } = await example.getFields();
+
+      this.contractFields = JSON.stringify({ fields, state }, null, 2);
+    },
+  },
+});
+</script>
